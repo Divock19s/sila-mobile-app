38c75b6ffcd38c044ef5b981fe1200c855a855ed
anas <anes85594@gmail.com>
2024-02-15 10:59:52 +0100

    fixed the signing background color

--- pages/SignPage.js
+++ pages/SignPage.js
@@ -1,11 +1,11 @@
-import { View, Text, ImageBackground } from 'react-native';
+import { View } from 'react-native';
 import Sign from '../components/Sign';
 
 const SignPage = () => {
   return (
-    <ImageBackground source={require('../assets/images&logos/background-gradient-lights.jpg')} style={[{flex: 1}]}>
+    <View style={[{flex: 1}, {backgroundColor: '#7538D4'}]}>
       <Sign />
-    </ImageBackground>
+    </View>
   )
 }
 
